9edd33dfd4678a1914b5a1b70d969c548dedf310
Laztaitai <llateedah@gmail.com>
2023-01-23 11:26:46 +0000

    index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-"""Creation of API endpoints"""
-
+"""
+Flask route that returns json status response
+"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request
 from models import storage
-# Importing classes from models directory
-from models.amenity import Amenity
-from models.city import City
-from models.user import User
-from models.review import Review
-from models.place import Place
-from models.state import State
 
 
-@app_views.route('/status', strict_slashes=False)
-def status_api():
-    """Returning API status"""
-    return (jsonify({"status": "OK"}))
+@app_views.route('/status', methods=['GET'])
+def status():
+    """
+    function for status route that returns the status
+    """
+    if request.method == 'GET':
+        resp = {"status": "OK"}
+        return jsonify(resp)
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats_api():
-    """Endpoint that retrieves the number of each objects by type"""
-    return (jsonify({"amenities": storage.count(Amenity),
-                     "cities": storage.count(City),
-                     "places": storage.count(Place),
-                     "reviews": storage.count(Review),
-                     "states": storage.count(State),
-                     "users": storage.count(User)}))
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
