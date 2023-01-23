846ebe9a0a9ab499181241da43cfa6985e2307d6
Laztaitai <llateedah@gmail.com>
2023-01-23 08:19:24 +0000

    v1 files

---
+++ api/v1/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Endpoint (route) will be to return the status of Api"""
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask import *
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """closes the current sqlalchemy session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    Create a handler for 404 errors that returns a
+    JSON-formatted 404 status code response
+    """
+    return jsonify(error="Not found"), 404
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST') or '0.0.0.0'
+    port = getenv('HBNB_API_PORT') or '5000'
+    app.run(host=host, port=port, threaded=True)
