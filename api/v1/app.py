5467e2beb62a7226f2bb319f3291cd8f08f0a288
gillohsylvia <abdallahfatima363@gmail.com>
2023-01-25 04:49:42 +0300

    fixing

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""Endpoint (route) will be to return the status of Api"""
+"""
+Flask App that integrates with AirBnb static HTML Template
+"""
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask import *
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """closes the current sqlalchemy session"""
+def tear_down(exc):
+    """
+    this request closes the SQLAlchemy session after every request
+    """
     storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """
-    Create a handler for 404 errors that returns a
-    JSON-formatted 404 status code response
-    """
     return jsonify(error="Not found"), 404
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     host = getenv('HBNB_API_HOST') or '0.0.0.0'
-    port = getenv('HBNB_API_PORT') or '5000'
+    port = getenv('HBNB_API_PORT') or 5000
     app.run(host=host, port=port, threaded=True)
